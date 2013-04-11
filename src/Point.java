35114521d2c5d742842be061d6d2ac18b9924ea3
Thierry <thierry.leydier@gmail.com>
2013-04-11 18:58:46 +0200

    Première version

---
+++ src/Point.java
@@ -0,0 +1,29 @@
+
+public class Point {
+
+	double x, y ;
+	
+	public Point(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distance (Point p) {
+		double dx=x-p.x;
+		double dy=y-p.y;
+		return Math.sqrt(dx*dx+dy*dy) ;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Point I = new Point(1, 0) ;
+		Point J = new Point(0, 1) ;
+		
+		System.out.println ("D(I,J)="+I.distance(J)) ;
+	}
+
+}
